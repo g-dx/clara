68d54e93d0566b2346186ac725d85d542b0742f3
g-dx <g-dx@users.noreply.github.com>
2015-08-31 17:39:29 +0100

    Added code gen notes.

---
+++ src/github.com/g-dx/hello/cgen.go
@@ -0,0 +1,21 @@
+package main
+
+/**
+Code Generation:
+
+WINDOWS TODO:
+
+ - Need to generate:
+  * .rdata section with all string literals
+  * .text section with entry point and all function definitions
+
+MACH-O TODO:
+
+- Need to generate:
+  *
+
+*/
+
+func cgen() {
+
+}
