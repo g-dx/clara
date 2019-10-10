1f3222cf4d1204f2b95cd1469875b8e72302a67a
g-dx <g-dx@users.noreply.github.com>
2019-10-10 14:09:11 +0100

    Add new AST walking code
    The current code walks every type of AST node in the same way. This is leads to a confusing visit order for various types. The visit function itself is also overly complicated.
    
    This commit introduces a pair of new walk functions to support pre & post-order walks which visits a node based on its type using a much simpler visitor function. The intention is that this will replace the existing walk code.

---
+++ walk.go
@@ -0,0 +1,94 @@
+package main
+
+import "fmt"
+
+func WalkPreOrder(n *Node, f func(*Node) bool) {
+	Walk(true, n, f)
+}
+
+func WalkPostOrder(n *Node, f func(*Node)) {
+	Walk(false, n, func(n *Node) bool {
+		f(n)
+		return true
+	})
+}
+
+func Walk(isPreOrder bool, n *Node, f func(*Node) bool) {
+	if isPreOrder && !f(n) {
+		return
+	}
+
+	switch n.op {
+	case opBlockFnDcl, opExprFnDcl, opExternFnDcl, opConsFnDcl, opFuncType:
+		for _, param := range n.params {
+			Walk(isPreOrder, param, f)
+		}
+		for _, stmt := range n.stmts {
+			Walk(isPreOrder, stmt, f)
+		}
+		if n.left != nil {
+			Walk(isPreOrder, n.left, f)
+		}
+
+	case opRoot, opStructDcl, opEnumDcl, opBlock, opElse:
+		for _, stmt := range n.stmts {
+			Walk(isPreOrder, stmt, f)
+		}
+
+	case opFuncCall:
+		if n.left != nil {
+			Walk(isPreOrder, n.left, f)
+		}
+		for _, stmt := range n.stmts {
+			Walk(isPreOrder, stmt, f)
+		}
+
+	case opWhile, opMatch:
+		Walk(isPreOrder, n.left, f)
+		for _, stmt := range n.stmts {
+			Walk(isPreOrder, stmt, f)
+		}
+
+	case opCase:
+		for _, p := range n.params {
+			Walk(isPreOrder, p, f)
+		}
+		for _, stmt := range n.stmts {
+			Walk(isPreOrder, stmt, f)
+		}
+
+	case opIf, opElseIf:
+		Walk(isPreOrder, n.left, f)
+		for _, stmt := range n.stmts {
+			Walk(isPreOrder, stmt, f)
+		}
+		if n.right != nil {
+			Walk(isPreOrder, n.right, f)
+		}
+
+	case opLit, opError, opNamedType:
+		// ...
+
+	case opIdentifier, opReturn:
+		if n.left != nil {
+			Walk(isPreOrder, n.left, f)
+		}
+
+	case opNot, opNeg, opArrayType:
+		Walk(isPreOrder, n.left, f)
+
+	case opAs, opDas, opAdd, opSub, opMul, opDiv, opAnd, opOr, opBAnd,
+		opBOr, opBXor, opEq, opGt, opLt, opBLeft, opBRight, opDot, opArray:
+		Walk(isPreOrder, n.left, f)
+		Walk(isPreOrder, n.right, f)
+
+	default:
+		panic(fmt.Sprintf("Unexpected node type: %v", nodeTypes[n.op]))
+	}
+
+	if isPreOrder {
+		f(nil) // Signal node exit
+	} else {
+		f(n)
+	}
+}
